ee7aea1b0889fdfa20e9b2cb32c6be628f092360
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-07-30 13:23:33 -0700

    Add new recipe creation with tests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,36 @@
 class RecipesController < ApplicationController
+  before_action :get_recipe, only: [:show]
+
   def index
     @recipes = Recipe.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.first
+
+    if @recipe.save
+      flash[:success] = 'Your recipe has been successfullly created.'
+      redirect_to @recipe
+    else
+      render :new
+    end
+  end
+
+  private
+  def get_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def capitalize_words(words)
+    words.split.map(&:capitalize).join(' ')
+  end
+
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.chefname, class: 'rounded_corners')
+  end
 end
