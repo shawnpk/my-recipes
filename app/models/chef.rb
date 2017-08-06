634cfecda15987abc19946c86cf4bf9f89fdc4dc
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-08-06 08:51:59 -0700

    Add comment model and associations

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,6 @@
 class Chef < ApplicationRecord
-  has_many :recipes, dependent: :destroy
+  has_many :recipes,  dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   has_secure_password
 
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :chef
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 5, maximum: 500 }
